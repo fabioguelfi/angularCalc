7ccba1247a10c8a6014ea822c6b81a896319534d
fabioguelfi <fabioguelfiunix@gmail.com>
2018-04-25 01:54:29 -0300

    Criando o módulo Calculadora

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,3 +1,4 @@
+import { CalculadoraModule } from './calculadora';
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CalculadoraModule
   ],
   providers: [],
   bootstrap: [AppComponent]
